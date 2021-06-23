cf0809f9760adb61146a9e6f730a5d754a573d3a
Venky <18035237+kaluva-venky@users.noreply.github.com>
2021-06-23 17:51:19 -0500

    add error handling

--- hello/hello.go
+++ hello/hello.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"log"
+
 	"rsc.io/quote"
 
 	greetings "github.com/kaluva-venky/greetings-go"
@@ -10,8 +12,22 @@ import (
 
 func main() {
 
-	// Get a greeting message and print it.
-	message := greetings.Hello("Venky")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 	fmt.Println(quote.Go())
 }
