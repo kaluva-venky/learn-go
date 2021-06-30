822e8a0ccc1a223e6eda54d3030c83c39bd18679
Venky <18035237+kaluva-venky@users.noreply.github.com>
2021-06-30 12:34:24 -0500

    Using built-in maps
    implementing a new function for new/changed functionality in a module

--- hello/hello.go
+++ hello/hello.go
@@ -17,9 +17,11 @@ func main() {
 	// the time, source file, and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
 
-	// Request a greeting message.
-	message, err := greetings.Hello("Venky")
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
@@ -28,6 +30,6 @@ func main() {
 
 	// If no error was returned, print the returned message
 	// to the console.
-	fmt.Println(message)
+	fmt.Println(messages)
 	fmt.Println(quote.Go())
 }
